353a1c156cd4f477dc8ebbfdd28b55276331b868
superburt <53563328+superburt@users.noreply.github.com>
2020-08-18 11:01:50 +1000

    updated week 4 for task 3

--- Week4/week4/src/app/app.component.ts
+++ Week4/week4/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import {FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: []
+  bootstrap: [AppComponent]
+})
+
 export class AppComponent {
   title = 'week4';
 }
